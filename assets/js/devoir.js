d7af6b551f24efcd7c30c757c815e8e6ce87aae1
Chiainve <156919641+Chiainve@users.noreply.github.com>
2024-02-02 13:11:17 +0100

    devoirs

--- assets/js/devoir.js
+++ assets/js/devoir.js
@@ -1,1 +1,34 @@
-const pi = 3,14;
+
+
+/*
+const pi = 3.14;
+pi = 6.20;
+*/
+
+/*
+var a = new Number(50);
+let l = new Boolean(true);
+var c; 
+let k = { key1: 'value1'};
+
+
+console.log(typeof(a));
+console.log(typeof(l));
+console.log(typeof(c));
+console.log(typeof(k));
+*/
+/*
+var o = new Number(5);
+var z = new Number(25);
+
+o = o*z;
+
+console.log(o);
+*/
+let longueur = 30;
+let largeur = 10; 
+let perimetre = longueur * 2 + largeur * 2;
+
+
+console.log(perimetre);
+
